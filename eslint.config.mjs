7ab3e54729c375db2e6e6b0564de6d59506971c9
wiseandy32 <wiseandy32@gmail.com>
2025-10-09 02:25:40 +0000

    disable react/no-unescaped-entities rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { off } from "process";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -19,6 +20,9 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      'react/no-unescaped-entities': off,
+        },
   },
 ];
 
