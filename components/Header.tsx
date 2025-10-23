da23c987206d370dcea96442f4c3cdc0962ff10e
wiseandy32 <wiseandy32@gmail.com>
2025-10-23 16:51:16 +0000

    remove protected routes from mobile nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -83,18 +83,6 @@ export default function Header() {
         <div className="lg:hidden">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
             <Link
-              href="/dashboard"
-              className="block text-sm text-gray-300 hover:text-white px-3 py-2 rounded-md"
-            >
-              Dashboard
-            </Link>
-            <Link
-              href="/admin"
-              className="block text-sm text-gray-300 hover:text-white px-3 py-2 rounded-md"
-            >
-              Admin
-            </Link>
-            <Link
               href="/about-us"
               className="block text-sm text-gray-300 hover:text-white px-3 py-2 rounded-md"
             >
